c1277d3dcd6d7d9d4052b7b70a6fd775c5003d07
John Harrington <jxh01753@protonmail.com>
2018-11-15 14:39:35 +0000

    video 5

---
+++ frontend/.next/static/webpack/static/development/pages/_app.js.ada8157324f1f01cf4dd.hot-update.js
@@ -0,0 +1,58 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./components/Nav.js":
+/*!***************************!*\
+  !*** ./components/Nav.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/john-mbp/dev/wb-ar/frontend/components/Nav.js";
+
+
+var Nav = function Nav() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Link, {
+    href: "/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "Home!")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Link, {
+    href: "/sell",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, "Sell!")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Nav);
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.ada8157324f1f01cf4dd.hot-update.js.map
